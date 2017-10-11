d49406719b1dfbe16e8fc1a6970b693dbfeaa9ca
Johan Blomgren <spec4jb@hotmail.com>
2017-10-11 19:59:56 +0200

    remove stuff gitignore

--- Relab/Scripts/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var React = require("react");
-var ReactDOM = require("react-dom");
-var Test_1 = require("./components/Test/Test");
-ReactDOM.render(React.createElement(Test_1.Test, { title: "testme" }), document.getElementById("example"));
-//# sourceMappingURL=index.js.map
